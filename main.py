c8efcd76a6dc7d5b9bd5e20110df9e8101e7ef69
Luca Pereira <pereluca10@gmail.com>
2020-08-01 01:11:26 -0400

    server number tracking

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix='>')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name='>catfact'))
+    await client.change_presence(activity=discord.Game(name=f'>catfact | In {len(client.guilds)} servers!'))
     print(f'Bot has logged in at {datetime.datetime.now().time()}\nLatency is {round(client.latency * 1000)}ms')
 
 for filename in os.listdir('./cogs'):
