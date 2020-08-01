5fc059b4662a050884f8eed1e7cf11185856c7b2
Luca Pereira <pereluca10@gmail.com>
2020-08-01 00:27:58 -0400

    removed 'support' alias from github

--- cogs/github.py
+++ cogs/github.py
@@ -7,7 +7,7 @@ class Github(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['git', 'support', 'issue'])
+    @commands.command(aliases=['git', 'issue'])
     async def github(self, ctx):
         """Official github page"""
         embed = discord.Embed(title='Github', description='The official catfacts github page. https://github.com/mlucap/catfacts', url='https://github.com/mlucap/catfacts', colour=discord.Color.blue())
