ca270ffe4748ac41a25fcb8241f026616a04b651
Luca Pereira <pereluca10@gmail.com>
2020-08-02 19:02:54 -0400

    Changed to an embed

--- cogs/catfact.py
+++ cogs/catfact.py
@@ -21,18 +21,13 @@ class Cat(commands.Cog):
         """Gets a random cat fact."""
 
         await ctx.trigger_typing()
-
-        try:
-            async with aiohttp.ClientSession().get("https://catfact.ninja/fact") as response:
-                fact = (await response.json())["fact"]
-                length = (await response.json())["length"]
-                await ctx.send(f'Random cat fact number {length}:\n{fact}')
-            await aiohttp.ClientSession().close()
-                
-        except aiohttp.ClientError:
-            logging.error('Api call error')
-            await ctx.send("`API call error. Unable to fetch catfact. Details in console, if this persists, make a bugreport `>bug [information]`.`")
-
+        async with aiohttp.ClientSession().get("https://catfact.ninja/fact") as response:
+        
+            fact = (await response.json())["fact"]
+            length = (await response.json())["length"]
+            embed = discord.Embed(title=f'Random cat fact number: **{length}**', description=f'{fact}', colour=0x400080)
+            # await ctx.send(f'Random cat fact number {length}:\n{fact}')
+            await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Cat(client))
