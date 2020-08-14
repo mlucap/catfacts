4bc714354fb143af18fb99084b96bad5e88e4cc9
Luca Pereira <pereluca10@gmail.com>
2020-08-14 19:05:35 -0400

    re-added error.py, added changeprefix command to help

--- cogs/help.py
+++ cogs/help.py
@@ -12,6 +12,7 @@ class Help(commands.Cog):
         """Shows this menu"""
         embed = discord.Embed(title="Help Menu", colour=discord.Color.green())
         embed.add_field(name="Catfact", value="Gives you a new, random catfact every command run!")
+        embed.add_field(name="Changeprefix", value="Reserved for server admins only. Allows for admins to change the prefix for their server.")
         embed.add_field(name="Bug", value="Reports any bugs you may come across to the owner (Missue of this command may result in a blacklist from running commands.)")
         embed.add_field(name="Support", value="Provides you with different ways to support the project.")
         embed.add_field(name="Github", value="Provides you with the official github repository of this bot.")
